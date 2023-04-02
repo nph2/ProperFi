02cfd041488f7a7174212db1eb99865b1ecd67fd
nph2 <nph2@princeton.edu>
2023-04-02 03:35:01 -0400

    yee

--- index/src/components/Listings.jsx
+++ index/src/components/Listings.jsx
@@ -38,7 +38,7 @@ const Listings = () => (
             </div>
 
             <div className="flex items-center justify-between">
-              <Buttonz className="bg-[#01f6ff] text-black font-poppins font-medium py-2 px-4 rounded-lg" to="https://goprincetontigers.com/sports/mens-water-polo" location={location}/>
+              <Buttonz className="bg-[#01f6ff] text-black font-poppins font-medium py-2 px-4 rounded-lg" to="https://properfi-mint.vercel.app/" location={location}/>
               <h4 className="font-poppins font-semibold text-base text-white">$542/TKN</h4>
             </div>
 
